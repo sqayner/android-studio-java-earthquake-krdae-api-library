c5ec62142119d1d31cc0c8df8344837ea919bdd4
Şevket Eren KAYNAR <sqayner@gmail.com>
2023-08-07 12:46:38 +0300

    2.0

--- app/src/main/java/com/sqayner/library/earthquake/krdae/MainActivity.java
+++ app/src/main/java/com/sqayner/library/earthquake/krdae/MainActivity.java
@@ -1,9 +1,9 @@
 package com.sqayner.library.earthquake.krdae;
 
+import androidx.appcompat.app.AppCompatActivity;
+
 import android.os.Bundle;
 import android.widget.Toast;
-
-import androidx.appcompat.app.AppCompatActivity;
 
 import com.sqayner.library.earthquake.krdae.model.KrdaeEarthquakeModel;
 
